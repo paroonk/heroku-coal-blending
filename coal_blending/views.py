5c096f0b1992c003c92e52f188b8c4e2b71d0ec1
Paroon <paroonk@hotmail.com>
2021-09-22 11:59:51 +0700

    1

--- coal_blending/views.py
+++ coal_blending/views.py
@@ -14,5 +14,5 @@ class DashboardView(generic.TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
-        
+        context['Test'] = 'Test123'
         return context
